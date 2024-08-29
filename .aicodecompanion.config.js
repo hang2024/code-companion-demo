26c6f466c5ea48ea18ab4207daaa790d6a6d3549
Matt Bryant <matt.bryant@spatialfront.com>
2024-08-29 11:04:22 -0500

    Added Code Companion Config File

---
+++ .aicodecompanion.config.js
@@ -0,0 +1,16 @@
+// Example configuration for providing the API Endpoint:
+module.exports = {
+    apiProvider: {
+      AzureOpenAI: {
+        endPointUrl:
+          "https://sfi-azureopenai-test.openai.azure.com/openai/deployments/ChatGPT35Turbo/chat/completions?api-version=2024-04-01-preview",
+      },
+      // Below is reference for OpenAI and PSChat Providers in case you want to change the URL for these Options. It will work only if Request and Response Contracts are same for your endpoints
+      OpenAI: {
+        endPointUrl: "Open AI URL",
+      },
+      PSChat: {
+        endPointUrl: "PSChat URL",
+      },
+    },
+  };
