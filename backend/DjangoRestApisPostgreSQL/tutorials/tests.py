b019df9f654b0b763c602d8738bfb0bdb03b90e8
Matt Bryant <matt.bryant@spatialfront.com>
2024-09-09 14:32:47 -0500

    Updated HTML and CSS for USWDS and Unit Tests

--- backend/DjangoRestApisPostgreSQL/tutorials/tests.py
+++ backend/DjangoRestApisPostgreSQL/tutorials/tests.py
@@ -1,3 +1,91 @@
-from django.test import TestCase
+# tests.py
 
-# Create your tests here.
+from django.test import TestCase, Client
+from django.urls import reverse
+from rest_framework import status
+from tutorials.models import Tutorial
+from tutorials.serializers import TutorialSerializer
+import json
+
+client = Client()
+
+class TutorialTests(TestCase):
+    def setUp(self):
+        self.tutorial1 = Tutorial.objects.create(title='Tutorial 1', description='Description 1', published=True)
+        self.tutorial2 = Tutorial.objects.create(title='Tutorial 2', description='Description 2', published=False)
+        self.valid_payload = {
+            'title': 'Tutorial 3',
+            'description': 'Description 3',
+            'published': True
+        }
+        self.invalid_payload = {
+            'title': '',
+            'description': 'Description 3',
+            'published': True
+        }
+
+    def test_get_all_tutorials(self):
+        response = client.get(reverse('tutorial_list'))
+        tutorials = Tutorial.objects.all()
+        serializer = TutorialSerializer(tutorials, many=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json(), serializer.data)
+
+    def test_create_valid_tutorial(self):
+        response = client.post(
+            reverse('tutorial_list'),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_invalid_tutorial(self):
+        response = client.post(
+            reverse('tutorial_list'),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_delete_all_tutorials(self):
+        response = client.delete(reverse('tutorial_list'))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Tutorial.objects.count(), 0)
+
+    def test_get_valid_single_tutorial(self):
+        response = client.get(reverse('tutorial_detail', kwargs={'pk': self.tutorial1.pk}))
+        tutorial = Tutorial.objects.get(pk=self.tutorial1.pk)
+        serializer = TutorialSerializer(tutorial)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json(), serializer.data)
+
+    def test_get_invalid_single_tutorial(self):
+        response = client.get(reverse('tutorial_detail', kwargs={'pk': 999}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_update_valid_tutorial(self):
+        response = client.put(
+            reverse('tutorial_detail', kwargs={'pk': self.tutorial1.pk}),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_invalid_tutorial(self):
+        response = client.put(
+            reverse('tutorial_detail', kwargs={'pk': self.tutorial1.pk}),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_delete_valid_tutorial(self):
+        response = client.delete(reverse('tutorial_detail', kwargs={'pk': self.tutorial1.pk}))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_get_published_tutorials(self):
+        response = client.get(reverse('tutorial_list_published'))
+        tutorials = Tutorial.objects.filter(published=True)
+        serializer = TutorialSerializer(tutorials, many=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json(), serializer.data)
--- frontend/src/app/components/tutorials-list/tutorials-list.component.ts
+++ frontend/src/app/components/tutorials-list/tutorials-list.component.ts
@@ -2,23 +2,39 @@ import { Component, OnInit } from '@angular/core';
 import { Tutorial } from '../../models/tutorial.model';
 import { TutorialService } from '../../services/tutorial.service';
 
+/**
+ * @Component decorator provides metadata about the component, 
+ * such as its selector, template URL, and styles.
+ */
 @Component({
   selector: 'app-tutorials-list',
   templateUrl: './tutorials-list.component.html',
   styleUrls: ['./tutorials-list.component.css'],
 })
 export class TutorialsListComponent implements OnInit {
-  tutorials?: Tutorial[];
-  currentTutorial: Tutorial = {};
-  currentIndex = -1;
-  title = '';
+  tutorials?: Tutorial[]; // Array to hold the list of tutorials
+  currentTutorial: Tutorial = {}; // Object to hold the currently selected tutorial
+  currentIndex = -1; // Index of the currently selected tutorial
+  title = ''; // Title to search for tutorials
 
+  /**
+   * Constructor to inject the TutorialService dependency
+   * @param tutorialService - Service to handle tutorial-related operations
+   */
   constructor(private tutorialService: TutorialService) {}
 
+  /**
+   * Lifecycle hook that is called after data-bound properties of a directive are initialized.
+   * Here, it is used to retrieve the list of tutorials when the component is initialized.
+   */
   ngOnInit(): void {
     this.retrieveTutorials();
   }
 
+  /**
+   * Method to retrieve all tutorials from the service.
+   * It subscribes to the observable returned by the service and assigns the data to the tutorials array.
+   */
   retrieveTutorials(): void {
     this.tutorialService.getAll().subscribe({
       next: (data) => {
@@ -29,17 +45,30 @@ export class TutorialsListComponent implements OnInit {
     });
   }
 
+  /**
+   * Method to refresh the list of tutorials.
+   * It retrieves the tutorials again and resets the current tutorial and index.
+   */
   refreshList(): void {
     this.retrieveTutorials();
     this.currentTutorial = {};
     this.currentIndex = -1;
   }
 
+  /**
+   * Method to set the currently active tutorial.
+   * @param tutorial - The tutorial object to set as active
+   * @param index - The index of the tutorial in the list
+   */
   setActiveTutorial(tutorial: Tutorial, index: number): void {
     this.currentTutorial = tutorial;
     this.currentIndex = index;
   }
 
+  /**
+   * Method to remove all tutorials.
+   * It calls the service to delete all tutorials and refreshes the list upon success.
+   */
   removeAllTutorials(): void {
     this.tutorialService.deleteAll().subscribe({
       next: (res) => {
@@ -50,6 +79,10 @@ export class TutorialsListComponent implements OnInit {
     });
   }
 
+  /**
+   * Method to search for tutorials by title.
+   * It calls the service to find tutorials by title and assigns the data to the tutorials array.
+   */
   searchTitle(): void {
     this.currentTutorial = {};
     this.currentIndex = -1;
