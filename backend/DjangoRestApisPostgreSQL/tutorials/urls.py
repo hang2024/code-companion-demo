7e53cf9e2ae5c29e2b4a4ae64dde8376ce37c31b
Matt Bryant <matt.bryant@spatialfront.com>
2024-09-22 16:54:03 -0500

    Another basline fix

--- backend/DjangoRestApisPostgreSQL/tutorials/urls.py
+++ backend/DjangoRestApisPostgreSQL/tutorials/urls.py
@@ -3,7 +3,7 @@ from django.urls import re_path
 from tutorials import views 
  
 urlpatterns = [ 
-    re_path(r'^api/tutorials$', views.tutorial_list),
-    re_path(r'^api/tutorials/(?P<pk>[0-9]+)$', views.tutorial_detail),
-    re_path(r'^api/tutorials/published$', views.tutorial_list_published)
+    re_path(r'^api/tutorials$', views.tutorial_list, name='tutorial_list'),
+    re_path(r'^api/tutorials/(?P<pk>[0-9]+)$', views.tutorial_detail, name='tutorial_detail'),
+    re_path(r'^api/tutorials/published$', views.tutorial_list_published, name='tutorial_list_published')
 ]
