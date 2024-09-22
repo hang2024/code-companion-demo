d73820faf31d3031b0ca0bbe3ded9879bdd282bd
Matt Bryant <matt.bryant@spatialfront.com>
2024-09-22 12:49:34 -0500

    Fix for new baseline

--- backend/DjangoRestApisPostgreSQL/tutorials/urls.py
+++ backend/DjangoRestApisPostgreSQL/tutorials/urls.py
@@ -1,11 +1,9 @@
-# urls.py in your 'tutorials' app
+from django.urls import re_path
 
-from django.urls import path
-from tutorials import views
-
-urlpatterns = [
-    path('tutorials/', views.tutorial_list, name='tutorial_list'),
-    path('tutorials/<int:pk>/', views.tutorial_detail, name='tutorial_detail'),
-    path('tutorials/published/', views.tutorial_list_published, name='tutorial_list_published'),
+from tutorials import views 
+ 
+urlpatterns = [ 
+    re_path(r'^api/tutorials$', views.tutorial_list),
+    re_path(r'^api/tutorials/(?P<pk>[0-9]+)$', views.tutorial_detail),
+    re_path(r'^api/tutorials/published$', views.tutorial_list_published)
 ]
-
